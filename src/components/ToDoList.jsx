723ba5ef2e92a154569b6adbdfd88990b201537f
Vaishak-P <vaishakpanakkat@gmail.com>
2024-12-20 18:59:33 +0530

    fontawesome icon added,checked task conditional classname added

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,11 +1,33 @@
 import React,{useState} from 'react'
+//import icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+
 const ToDoList = () => {
 
     // const [tasks,setTasks] = useState(['eat','code','sleep']);
     const [tasks,setTasks] = useState([
         {id:1,text:'eat',completed:false},
         {id:2,text:'code',completed:false},
-        {id:3,text:'sleep',completed:false}
+        {id:3,text:'sleep',completed:false},
+        {id:4,text:'eat',completed:false},
+        {id:5,text:'code',completed:false},
+        {id:6,text:'sleep',completed:false},
+        {id:7,text:'eat',completed:false},
+        {id:8,text:'code',completed:false},
+        {id:9,text:'code',completed:false},
+        // {id:3,text:'sleep',completed:false},
+        // {id:1,text:'eat',completed:false},
+        // {id:2,text:'code',completed:false},
+        // {id:3,text:'sleep',completed:false},
+        // {id:1,text:'eat',completed:false},
+        // {id:2,text:'code',completed:false},
+        // {id:3,text:'sleep',completed:false}
     ]);
     const [newTask,setNewTask] = useState(""); 
     const [deletedTask,setDeletedTask] = useState("");
@@ -89,101 +111,124 @@ const ToDoList = () => {
     })
     
     return(
-        <>
-            <div className='toDoHead'>
-                <h1>Y2Do</h1>
-                <div>
-                    <input
-                        type='text'
-                        placeholder='Enter a task'
-                        value={newTask}
-                        onChange={(e)=>setNewTask(e.target.value)}
-                    />
-                    <button
-                        className='addButton'
-                        onClick = {addTask}
-                    >Add
-                    </button>
+        <div className='bodyContainer'>
+            <div className='mainContainer'>
+                {/* HEAD  */}
+                <div className='toDoHead'>
+                    <h1 className='y2doTitle'>Y2Do</h1>
+                    <div className='newTaskInputContainer'>
+                        <input
+                            type='text'
+                            placeholder='enter new task'
+                            value={newTask}
+                            onChange={(e)=>setNewTask(e.target.value)}
+                            onKeyDown={(e)=>{
+                                if(e.key==='Enter'){
+                                    addTask();
+                                }
+                            }}
+                            className='newTaskInput'
+                        />
+                        <button
+                            className='addButton'
+                            onClick = {addTask}
+                        >
+                            <FontAwesomeIcon icon={faPlus} />
+                        </button>
+                    </div>
                 </div>
-                <div className='searchContainer'>
-                    <input
-                        type='text'
-                        value={searchQuery}
-                        placeholder='Search ToDos'
-                        onChange={(e)=>setSearchQuery(e.target.value)}
-                    />
-                    <select value={filter} onChange={(e)=>setFilter(e.target.value)}>
-                            <option value='all'>All</option>
-                            <option value='active'>Active</option>
-                            <option value='completed'>Completed</option>
-                    </select>
+                {/* LIST  */}
+                <div className='toDoList'>
+                    <div className='searchContainer'>
+                        <input
+                            type='text'
+                            value={searchQuery}
+                            placeholder='Search ToDos'
+                            onChange={(e)=>setSearchQuery(e.target.value)}
+                            className='searchTaskInput'
+                        />
+                        <select value={filter} onChange={(e)=>setFilter(e.target.value)}>
+                                <option value='all'>All</option>
+                                <option value='active'>Active</option>
+                                <option value='completed'>Completed</option>
+                        </select>
+                    </div>
+                    <ol>
+                        {filteredTasks.map((task,index) => 
+                            <li key={task.id}>
+                                {editingIndex=== index? (
+                                    <>
+                                        <input
+                                            type='text'
+                                            value={editText}
+                                            onChange={(e)=> setEditText(e.target.value)}
+                                        />
+                                        <button 
+                                            id='saveEditButton'
+                                            className={task.completed?'listbuttonDisabled':'listButton'}
+                                            onClick={()=> saveEdit(index)}
+                                            >
+                                            <FontAwesomeIcon icon={faFloppyDisk}/>
+                                        </button>
+                                    </>
+                                ):(
+                                    <>
+                                        <span 
+                                            id='text'
+                                            onClick={()=> toggleTaskCompletion(index)}
+                                            className={task.completed? 'textCompletedTrue':'textCompletedFalse'}
+                                        >
+                                            {task.text}
+                                        </span>
+                                        <button 
+                                            id='editButton'
+                                            // id={task.completed? 'editButtonActive':'editButtonDisabled'}
+                                            className={task.completed?'listbuttonDisabled':'listButton'}
+                                            onClick={()=>editTask(index,task.text)}
+                                        >
+                                        <FontAwesomeIcon icon={faPenToSquare} />
+                                        </button> 
+                                        <button 
+                                            id='deleteButton'
+                                            className={task.completed?'listbuttonDisabled':'listButton'}
+                                            onClick={()=>deleteTask(index)}
+                                        >
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                        <button 
+                                            id='moveUpButton'
+                                            className={task.completed?'listbuttonDisabled':'listButton'}
+                                            onClick={()=>moveTaskUp(index)}
+                                        >
+                                            <FontAwesomeIcon icon={faArrowUp} />
+                                        </button>
+                                        <button 
+                                            id='moveDownButton'
+                                            className={task.completed?'listbuttonDisabled':'listButton'}
+                                            onClick={()=>moveTaskDown(index)}
+                                        >
+                                            <FontAwesomeIcon icon={faArrowDown} />
+                                        </button>
+                                    </>
+                                )}
+                            </li>
+                        )}
+                    </ol> 
                 </div>
+                {deletedTask&&(
+                    <div className='undoDeletedTask'>
+                        <p>Task {deletedTask.text} deleted</p>
+                        <button
+                            onClick={undoDeletedTask}
+                        >Undo
+                        </button>
+                    </div>
+                )}
             </div>
-            <div className='toDoList'>
-                <ol>
-                    {filteredTasks.map((task,index) => 
-                        <li key={task.id}>
-                            {editingIndex=== index? (
-                                <>
-                                    <input
-                                        type='text'
-                                        value={editText}
-                                        onChange={(e)=> setEditText(e.target.value)}
-                                    />
-                                    <button onClick={()=> saveEdit(index)}>Save</button>
-                                </>
-                            ):(
-                                <>
-                                    <span 
-                                        id='text'
-                                        onClick={()=> toggleTaskCompletion(index)}
-                                        className={task.completed? 'textCompletedTrue':'textCompletedFalse'}
-                                    >
-                                        {task.text}
-                                    </span>
-                                    <button 
-                                        id='editeButton'
-                                        className='listContent'
-                                        onClick={()=>editTask(index,task.text)}
-                                    >Edit
-                                    </button>
-                                    <button 
-                                        id='deleteButton'
-                                        className='listContent'
-                                        onClick={()=>deleteTask(index)}
-                                    >Delete
-                                    </button>
-                                    <button 
-                                        id='moveUpButton'
-                                        className='listContent'
-                                        onClick={()=>moveTaskUp(index)}
-                                    >Up
-                                    </button>
-                                    <button 
-                                        id='moveDownButton'
-                                        className='listContent'
-                                        onClick={()=>moveTaskDown(index)}
-                                    >Down
-                                    </button>
-                                </>
-                            )}
-                        </li>
-                    )}
-                </ol>
-            </div>
-            {deletedTask&&(
-                <div className='undoDeletedTask'>
-                    <p>Task {deletedTask.text} deleted</p>
-                    <button
-                        onClick={undoDeletedTask}
-                    >Undo
-                    </button>
-                </div>
-            )}
             <div className="toDoFooter">
                 <p>Made with ❤ by Vaishak</p>
             </div>
-        </>
+        </div>
     )
 }   
 export default ToDoList
